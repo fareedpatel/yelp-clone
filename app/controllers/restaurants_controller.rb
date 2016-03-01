3e42cb2cd64db6a7ee3371b3b78ec967c1d90278
fareedpatel <fareed_patel@hotmail.com>
2016-03-01 18:38:41 +0000

    UPDATING RESTAURANTS

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,10 +4,11 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+  	@restaurant = Restaurant.new
   end
 
   def create
-  	Restaurant.create(restaurant_params)
+  	@restaurant = Restaurant.create(restaurant_params)
   	redirect_to '/restaurants'
   end
 
@@ -15,5 +16,14 @@ class RestaurantsController < ApplicationController
   	params.require(:restaurant).permit(:name)
   end
 
+  def edit
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+  	@restaurant = Restaurant.find(params[:id])
+  	@restaurant.update(restaurant_params)
+  	redirect_to '/restaurants'
+  end
 end
 
